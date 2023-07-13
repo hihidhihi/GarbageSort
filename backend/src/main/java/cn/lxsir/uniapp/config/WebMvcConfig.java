63de22877218d8bc946c2a62842e6b9a92d824a8
hihidhihi <2604559691@qq.com>
2023-07-14 00:09:05 +0800

    First commit

--- backend/src/main/java/cn/lxsir/uniapp/config/WebMvcConfig.java
+++ backend/src/main/java/cn/lxsir/uniapp/config/WebMvcConfig.java
@@ -4,12 +4,18 @@ package cn.lxsir.uniapp.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("doc.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
         System.out.println("success");
     }
 }
